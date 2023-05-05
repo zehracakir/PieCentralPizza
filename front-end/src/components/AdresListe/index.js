c4326d6bbffba14b9824d5aa507b4e0713e58fc9
YusufDagdeviren <ddyusufdd@gmail.com>
2023-05-05 18:32:51 +0300

    Address List edited

--- front-end/src/components/AdresListe/index.js
+++ front-end/src/components/AdresListe/index.js
@@ -1,44 +1,19 @@
-import { useState } from 'react'
+import React from 'react'
 import {
-  Box,
-  List,
   ListItem,
   Typography,
   ListItemText,
-  Divider,
   IconButton
 } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
-import AdresDuzenle from '../AdresDuzenle'
-function AdresListe() {
-  const [open, setOpen] = useState(false)
-  const handleClose = () => {
-    setOpen(false);
-  }
-  const handleClickOpen = () => {
-    setOpen(true);
-  }
+function AdresListe({ handleClickOpen, adresIsmi, adres }) {
   return (
-    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <List sx={{ maxWidth: "70%" }}>
-        <Divider />
-        <ListItem disablePadding sx={{ maxWidth: "100%" }}>
-          <ListItemText primary={<Typography sx={{ fontWeight: "bold", fontFamily: "Sansserif" }}>Ev</Typography>} secondary={<Typography variant='p' sx={{ color: "#757575" }}>Soğucak Mh, 2, Kuşadası Aydın</Typography>} />
-          <IconButton edge="end" aria-label="comments" onClick={handleClickOpen}>
-            <EditIcon />
-          </IconButton>
-        </ListItem>
-        <Divider />
-        <ListItem disablePadding>
-          <ListItemText primary={<Typography sx={{ fontWeight: "bold", fontFamily: "Sansserif" }}>Okul</Typography>} secondary={<Typography variant='p' sx={{ color: "#757575" }}>Adalet Mh, Parkin içi, Osmangazi Bursa</Typography>} />
-          <IconButton edge="end" aria-label="comments" onClick={handleClickOpen}>
-            <EditIcon />
-          </IconButton>
-        </ListItem>
-        <Divider />
-      </List>
-      <AdresDuzenle open={open} handleClose={handleClose} />
-    </Box>
+    <ListItem disablePadding sx={{ maxWidth: "100%" }}>
+      <ListItemText primary={<Typography sx={{ fontWeight: "bold", fontFamily: "Sansserif" }}>{adresIsmi}</Typography>} secondary={<Typography variant='p' sx={{ color: "#757575" }}>{adres}</Typography>} />
+      <IconButton edge="end" aria-label="comments" onClick={handleClickOpen}>
+        <EditIcon />
+      </IconButton>
+    </ListItem>
   )
 }
 
--- front-end/src/pages/Adreslerim/index.js
+++ front-end/src/pages/Adreslerim/index.js
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import {
     Typography,
     Box,
-    Button
+    Button,
+    List,
+    Divider
 } from '@mui/material'
 import AddLocationIcon from '@mui/icons-material/AddLocation';
 import AdresEkle from '../../components/AdresEkle';
 import AdresListe from '../../components/AdresListe';
+import AdresDuzenle from '../../components/AdresDuzenle';
 function Adreslerim() {
     const [open, setOpen] = useState(false)
     const handleClose = () => {
@@ -19,11 +22,20 @@ function Adreslerim() {
         <Box sx={{ display: "flex", flexDirection: "column" }}>
             <Typography variant='h5' mb={2} sx={{ fontWeight: 'bold' }}>Adreslerim</Typography>
             {/* <Typography variant='p' sx={{ fontFamily: "sans-serif" }}>Adres Eklenmedi</Typography> */}
-            <AdresListe />
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <List sx={{ maxWidth: "70%" }}>
+                    <Divider />
+                    <AdresListe handleClickOpen={handleClickOpen} adresIsmi={"Ev"} adres={"Soğucak Mh, 2, Kuşadası Aydın"} />
+                    <Divider />
+                    <AdresListe handleClickOpen={handleClickOpen} adresIsmi={"İş Yeri"} adres={"Yukarı dikmen mh, 664, Çankaya Ankara"} />
+                    <Divider />
+                </List>
+            </Box>
             <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
                 <Button sx={{ mt: 1, textTransform: "none", color: "#019f00", fontWeight: "bold" }} variant="text" startIcon={<AddLocationIcon />} onClick={handleClickOpen}>Yeni Adres Ekle</Button>
             </Box>
             <AdresEkle open={open} setOpen={setOpen} handleClose={handleClose} />
+            <AdresDuzenle open={open} handleClose={handleClose} />
         </Box>
     )
 }
