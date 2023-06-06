c70a366d2f224078330bca5342c20ef4c2a3a0ef
zehracakir <cakirzehra312@gmail.com>
2023-06-06 11:47:37 +0300

    enYeniKayittanSirala function added

--- back-end/src/controllers/Kullanici.js
+++ back-end/src/controllers/Kullanici.js
@@ -31,7 +31,32 @@ const buyuktenKucugeSirala = async function (req, res) {
       };
       
 
+      
+      const enYeniKayittanSirala = async function (req, res) {
+        try {
+          const kullaniciListesi = await KullaniciSema.find().sort({ kayitTarihi: -1 });
+          const siralanmisKullanicilar = kullaniciListesi.map(kullanici => kullanici.toObject());
+          cevapOlustur(res, 200, siralanmisKullanicilar);
+        } catch (err) {
+          cevapOlustur(res, 500, { error: 'Bir hata oluştu' });
+        }
+      };
+
+
+    //   const enEskiKayittanSirala = async function (req, res) {
+    //     try {
+    //       const kullaniciListesi = await KullaniciSema.find().sort({ kayitTarihi: 1 });
+    //       const siralanmisKullanicilar = kullaniciListesi.map(kullanici => kullanici.toObject());
+    //       cevapOlustur(res, 200, siralanmisKullanicilar);
+    //     } catch (err) {
+    //       cevapOlustur(res, 500, { error: 'Bir hata oluştu' });
+    //     }
+    //   };
+      
+
 module.exports = {
   buyuktenKucugeSirala,
-  kucuktenBuyugeSirala
+  kucuktenBuyugeSirala,
+  enYeniKayittanSirala,
+//   enEskiKayittanSirala
  }
--- back-end/src/routes/KullaniciRota.js
+++ back-end/src/routes/KullaniciRota.js
@@ -6,9 +6,16 @@ router
 .route('/buyukten-kucuge')
 .get(ctrlKullanicilar.buyuktenKucugeSirala)   //tum kullanicilari z den a ya siralama
 
-
 router
 .route('/kucukten-buyuge')
 .get(ctrlKullanicilar.kucuktenBuyugeSirala)   //tum kullanicilari a dan z ye siralama
+
+router
+.route('/en-yeni-kayittan-sirala')
+.get(ctrlKullanicilar.enYeniKayittanSirala)   //tum kullanicilari en yeni kayit olandan en eski kayit olana dogru siralama
+
+// router
+// .route('/en-eski-kayittan-sirala')
+// .get(ctrlKullanicilar.enEskiKayittanSirala)   //tum kullanicilari en eski kayit olandan en yeni kayit olana dogru siralama
 
 module.exports = router;
