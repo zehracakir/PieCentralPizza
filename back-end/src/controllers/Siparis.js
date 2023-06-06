97b3a4160d2fdd969cd1562b52612e15e64aa869
zehracakir <cakirzehra312@gmail.com>
2023-06-06 16:28:51 +0300

    durumaGoreSiparisGetir function added

--- back-end/src/controllers/Siparis.js
+++ back-end/src/controllers/Siparis.js
@@ -14,9 +14,35 @@ const tumSiparisleriGetir = async function (req, res) {
     cevapOlustur(res, 500, { error: 'Bir hata oluştu' });
     }
     };
-
-
+    
+    const durumaGoreSiparisGetir = async (req, res) => {
+        const siparisDurum = req.params.siparisDurum;
+      
+        try {
+          let siparisler,durum;
+          
+          if (siparisDurum === "teslim-edildi") {
+            durum = "Teslim edildi"
+          } else if (siparisDurum === "hazirlaniyor") {
+            durum ="Hazirlaniyor"
+          } else if (siparisDurum === "yolda") {
+            durum ="Yolda"
+          } else {
+            return cevapOlustur(res, 400, { "hata": "Böyle bir durum parametresi yok" });
+          }
+          siparisler = await SiparisSema.find({ siparisDurum: durum}).select("siparisDurum");
+          if (siparisler.length > 0) {
+            cevapOlustur(res, 200, siparisler);
+          } else {
+            cevapOlustur(res, 404, { "hata": "Duruma göre sipariş bulunamadı" });
+          }
+        } catch (error) {
+          cevapOlustur(res, 500, error);
+        }
+      };
+    
 
 module.exports = {
-    tumSiparisleriGetir
+    tumSiparisleriGetir,
+    durumaGoreSiparisGetir
  }
--- back-end/src/routes/SiparisRota.js
+++ back-end/src/routes/SiparisRota.js
@@ -6,5 +6,9 @@ router
 .route('/admin/siparisler')
 .get(ctrlSiparisler.tumSiparisleriGetir); //tum siparisleri listeleme (admin icin)
 
+router
+.route('/admin/siparisler/:siparisDurum')
+.get(ctrlSiparisler.durumaGoreSiparisGetir); //siparisleri durumlarina göre listeleme. hazirlaniyor/yolda/teslim-edildi (admin icin)
+
 
 module.exports = router;
