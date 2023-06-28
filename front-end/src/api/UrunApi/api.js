4b2b4dba9ec28e8b16fbc8db65e7395e73eff578
zehracakir <cakirzehra312@gmail.com>
2023-06-29 02:05:31 +0300

    Adding new product and fetching all products is complete. Product update and product deletion started.

--- front-end/src/api/UrunApi/api.js
+++ front-end/src/api/UrunApi/api.js
@@ -26,15 +26,26 @@ const urunDetayGetir = async (urunid) => {
     return data;
 }
 
-const yeniUrunEkle = async () => {
-    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/admin/urunler`);
+const yeniUrunEkle = async (input) => {
+    const { data } = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/admin/urunler`,input,setAuth());
     return data;
 }
 
 const adminTumUrunleriGetir = async () => {
-    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/urunler`);
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/urunler`, setAuth());
     return data;
 }
+
+const adminUrunGuncelle = async (urunid,values) => {
+    const { data } = await axios.put(`${process.env.REACT_APP_BASE_ENDPOINT}/admin/urunler/${urunid}`,values,setAuth());
+    return data;
+}
+
+const adminUrunSil=async (urunid) => {
+    const { data } = await axios.delete(`${process.env.REACT_APP_BASE_ENDPOINT}/admin/urunler/${urunid}`,setAuth());
+    return data;
+}
+ 
 
 export {
     kullaniciFavorileriGetir,
@@ -42,6 +53,8 @@ export {
     kategoriyeGoreUrunGetir,
     urunDetayGetir,
     yeniUrunEkle,
-    adminTumUrunleriGetir
+    adminTumUrunleriGetir,
+    adminUrunGuncelle,
+    adminUrunSil
 }
 
